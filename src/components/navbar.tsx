9a0bdb0b0df742322278c17bf0efe465c4ead062
Thanawich Juengkijthanawat <thanawich.jue@student.mahidol.edu>
2025-08-31 11:53:39 +0700

    responsive fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,12 +4,12 @@ export default function Navbar() {
     return (
     <>
     {/* Navbar */}
-    <nav className="bg-green-800 text-white px-8 py-4 flex justify-between items-center shadow-md">
-    <ul className="hidden md:flex gap-8 text-lg items-center">
+    <nav className="flex flex-col bg-green-800 text-white px-8 py-4 md:flex-row justify-between items-center shadow-md">
+    <ul className="md:flex gap-8 text-lg items-center">
     <div className="text-2xl font-bold tracking-wider">Job Jai</div>
       <Link to={`/`}>
           <li>
-            <a href="#" className="hover:text-yellow-300 transition-colors duration-300">
+            <a href="#" className="ml-2 hover:text-yellow-300 transition-colors duration-300">
             หน้าแรก
             </a>
           </li>
@@ -22,14 +22,14 @@ export default function Navbar() {
           </li>
       </Link>
     </ul>
-      <ul className='flex items-center gap-4'>
+    <ul className='flex flex-col gap-4 mt-4 md:mt-0 md:flex-row '>
           <Link to={`/applicant_tracking`}>
               <button className="bg-white text-[#1dc400] font-semibold px-5 py-2 rounded-xl hover:bg-[#1dc400]-100 focus:outline-none focus:ring-4 transition-all duration-300 ease-in-out transform hover:scale-105">
                 ติดตามผู้สมัคร
               </button>
           </Link>
         <Link to={`/register`}>
-          <button className="bg-[#1dc400] text-white font-semibold px-5 py-2 rounded-xl hover:bg-[#1dc400]-100 focus:outline-none focus:ring-4 transition-all duration-300 ease-in-out transform hover:scale-105">
+          <button className="ml-4 bg-[#1dc400] text-white font-semibold px-5 py-2 rounded-xl hover:bg-[#1dc400]-100 focus:outline-none focus:ring-4 transition-all duration-300 ease-in-out transform hover:scale-105">
               ลงทะเบียน
             </button>
         </Link>
